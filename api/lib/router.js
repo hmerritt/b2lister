f3ee9369cc58b0a4303c1be1750bc1b8c9624555
Harry Merritt <harry@merritt.es>
2020-10-05 06:22:40 +0100

    setup router with basic post data validation

---
+++ api/lib/router.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const app = require("./app");
+
+/*
+ * Returns list of bucket files
+ */
+router.post("/bucket/files", validatePostData(), async (req, res, next) => {
+    res.send({});
+});
+
+/*
+ * Validate post data
+ */
+function validatePostData() {
+    return (req, res, next) => {
+        if (req.body && req.body.bucket) {
+            next();
+        } else {
+            res.status(400).json({
+                error: "missing post data",
+                msg: "bucket: name-of-bucket"
+            });
+        }
+    };
+}
+
+module.exports = router;
