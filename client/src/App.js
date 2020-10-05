e84702f1d9b08927547f6619604046b580424008
Harry Merritt <harry@merritt.es>
2020-10-05 08:17:57 +0100

    setup App.js to render title and files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Files from "./components/Files/Files";
 
 function App() {
+    const [bucket, setBucket] = useState("collection-2001");
+
+    useEffect(() => {
+        document.title = `${bucket} | b2lister`;
+    }, [bucket]);
+
     return (
-        <div className="App">
-            <header className="App-header">b2lister</header>
+        <div className="App app-wrapper">
+            <div className="container">
+                <header className="App-header">
+                    <h1>b2lister</h1>
+                </header>
+
+                <section>
+                    <Files bucket={bucket} />
+                </section>
+            </div>
         </div>
     );
 }
